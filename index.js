720605b9b6f6123c77e19f8d853dd21cf800bec6
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 13:24:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name){
+  let kitten2 = [name];
+  var allKittens = kittens.concat(kitten2);
+  return allKittens
+}
+
+function prependKitten(name){
+  var moreKittens = kittens.unshift(name);
+  return moreKittens;
+}
